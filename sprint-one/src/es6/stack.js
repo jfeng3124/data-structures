28c1474331b1e127ae916f644c8ce39c7bec010b
Jia <jiafeng3124@gmail.com>
2019-02-20 12:31:02 -0800

    have something

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,22 +1,28 @@
-class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  constructor() {
-  	var stack = {};
-  	var push = function(string) {
-  		count = 0;
-  		for (let key in stack) {
-  			key = count;
-  			stack[key] = this.stack.push(string);
-  			count++;
-  		}
-  		return stack;
-  	}
-  	var pop = function() {
-  	  for (let key in stack) {
-  	    return stack[key].pop()
-  	  }
-  	}
-  	var size = stack.length
-  }
+var Stack = function() {
+    var count = 0;
+    var stack = {};
 }
+
+Stack.prototype.push = function(string) {
+    stack[count] = string;
+    count++;
+}
+
+Stack.prototype.pop = function() {
+
+    if (count === 0) {
+        return undefined;
+    }
+    count--;
+    var result = stack[count];
+    delete stack[count];
+    return result;
+}
+
+
+Stack.prototype.size = function() {
+    return count;
+}
+
+var stacks = Stack();
+console.log(stacks.push('string'))
