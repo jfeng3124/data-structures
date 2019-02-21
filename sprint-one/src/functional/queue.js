3c4cd169734c5432b6854fe8338ad5fec24f4567
Jia <jiafeng3124@gmail.com>
2019-02-20 17:47:12 -0800

    completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -16,27 +16,11 @@ var Queue = function() {
       return 0;
     }
     count--;
-    //iterate over the storage
-    //we want to delete the 0 index of the object
-    //return what is being deleted
-    for(var key in storage) {
-      var result = storage[key];
-      console.log('before storage[key] '+ storage[0])
-      delete result;
-      console.log('after delete ' + result)
-      console.log('after delete result ' + storage[key])
-      return storage[key];
+    var result = storage[0];
+    for(let i = 0; i < count; i++) {
+      storage[i] = storage[i+1]
+      return result;
     }
-    // for (var key in storage) {
-    //   var result = storage[key]
-    //   delete result;
-    //   console.log('storage[key]', result)
-    //   return storage[key]
-    // }
-    // var result = storage[0];
-    // console.log(storage[0])
-    // delete result;
-    // return result;
   };
 
   someInstance.size = function() {
