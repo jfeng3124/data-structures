4ea1e15dbf031ee821893ddf8e142a929ebc8bfd
Jia <jiafeng3124@gmail.com>
2019-02-20 23:54:45 -0800

    completed

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,27 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+  this.count = 0;
+  this.storage = {};
   }
+	enqueue (value) {
+    					this.storage[this.count] = value;
+    					this.count++;
+  					};
+
+	dequeue () {
+    					this.count--;
+    					var result = this.storage[0];
+    					for(let i = 0; i < this.count; i++) { 
+      					this.storage[i] = this.storage[i+1]
+    					}
+    					if (this.count === -1) {
+      					this.count = 0;
+    					}
+    					return result;
+  					};
+
+	size () {
+    		    return this.count;
+  				}
 
 }
