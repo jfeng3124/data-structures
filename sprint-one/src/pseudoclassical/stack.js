3c39215c31494f6246d87e7062ab5b9a7df4bb12
Jia <jiafeng3124@gmail.com>
2019-02-20 23:00:17 -0800

    semi-working code

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
+  this.push = function(value) {
+          this.storage[this.count] = value;
+    	    this.count++;
+  		  };
+
+	this.pop = function() {
+    		  if (this.count === 0) {
+      			this.count = 0;
+    	    }
+    			this.count--;
+    			var result = this.storage[this.count];
+    			delete this.storage[this.count];
+    			return result;
+        };
+	this.size = function() {
+    			return this.count
+  			}
+
 };
 
 
