1ec072222ad9d54c81782454c24c54d4b81242a9
Jia <jiafeng3124@gmail.com>
2019-02-24 13:59:23 -0800

    Added code from pairing

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,22 +1,49 @@
 var LinkedList = function() {
+  
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (list.head === null && list.tail === null) {
+      list.tail = node;
+      list.head = node;
+    }
+    else if (list.head !== null) {
+      list.tail.next = node; 
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var headNode = list.head.value; 
+    list.head = list.head.next; 
+    return headNode
   };
 
   list.contains = function(target) {
-  };
+    var node = list.head
+    var findTarget = function(node) {
+      if (node.value === target) {
+        return true;
+      }
+      if (node.next === null) {
+        return false;
+      }
+      node = node.next
+      return findTarget(node)
+    } 
+    return findTarget(node)
+  }
 
-  return list;
+
+return list;
 };
 
 var Node = function(value) {
   var node = {};
+
 
   node.value = value;
   node.next = null;
