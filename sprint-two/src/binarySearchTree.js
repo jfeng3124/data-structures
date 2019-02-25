56e878c33f7d76bc9941cbffc2694cbc1af60e64
Jia <jiafeng3124@gmail.com>
2019-02-24 23:57:28 -0800

    set up pseudoclassical implementation

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,20 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function (value) {
+
+};
+
+BinarySearchTree.prototype.contains = function (value) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (cb) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
